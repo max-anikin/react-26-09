fd21b537b51a06b9890c502e7deadb4684418851
Maksym_Anikin <max.anikin@gmail.com>
2022-12-19 19:43:18 +0200

    add h1 by jsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+//without jsx
+// const title = React.createElement(
+//     'h1',
+//     {
+//         id: 'title',
+//     },
+//     'Hello React!'
+// )
+
+//with jsx
+const title = <h1 id="title">Hello React (jsx)</h1>
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{title}</React.StrictMode>)
