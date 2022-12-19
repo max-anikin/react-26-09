433d914c5b3aa483f8cbe5ff9a46a01207641cce
Maksym_Anikin <max.anikin@gmail.com>
2022-12-19 19:55:53 +0200

    add Function component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const content = (
-    <div>
-        <ul>
-            <li>List Item 1</li>
-            <li>List Item 2</li>
-        </ul>
-    </div>
-)
+function App() {
+    return (
+        <div>
+            <h1>Hello React</h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum
+                expedita facilis quas veritatis quo nemo ad, necessitatibus
+                repellendus, sit ipsum saepe ullam rem asperiores voluptas iusto
+                magnam officiis a minima!
+            </p>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{content}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
