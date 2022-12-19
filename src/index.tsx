31cbe72e85dbd18e07c14f8f69d2148bf808df43
Maksym_Anikin <max.anikin@gmail.com>
2022-12-19 19:58:15 +0200

    add functional component by arrow function

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function App() {
+const App = () => {
     return (
         <div>
             <h1>Hello React</h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum
+                expedita facilis quas veritatis quo nemo ad, necessitatibus
+                repellendus, sit ipsum saepe ullam rem asperiores voluptas iusto
+                magnam officiis a minima!
+            </p>
             <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum
                 expedita facilis quas veritatis quo nemo ad, necessitatibus
