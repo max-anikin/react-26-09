fb0b73bc959b9f68cbd7e69d12c6e5fa2f6fbc17
Maksym_Anikin <max.anikin@gmail.com>
2022-12-19 20:13:58 +0200

    add React.Fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// const App = () => {
+//     return (
+//         <div>
+//             <h1>Hello React</h1>
+//             <p>
+//                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum
+//                 expedita facilis quas veritatis quo nemo ad, necessitatibus
+//                 repellendus, sit ipsum saepe ullam rem asperiores voluptas iusto
+//                 magnam officiis a minima!
+//             </p>
+// ;<p>
+//     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum expedita
+//     facilis quas veritatis quo nemo ad, necessitatibus repellendus, sit ipsum
+//     saepe ullam rem asperiores voluptas iusto magnam officiis a minima!
+// </p>
+//         </div>
+//     )
+// }
+
+const Title = () => {
+    return <h1>Hello React</h1>
+}
+
+const Content = () => {
+    return (
+        <React.Fragment>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Necessitatibus officia architecto repellendus at nulla nam
+                quibusdam esse? Aut dicta animi sed quos voluptatibus ad, cum
+                inventore recusandae non quis eligendi!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Necessitatibus officia architecto repellendus at nulla nam
+                quibusdam esse? Aut dicta animi sed quos voluptatibus ad, cum
+                inventore recusandae non quis eligendi!
+            </p>
+        </React.Fragment>
+    )
+}
+
 const App = () => {
     return (
-        <div>
-            <h1>Hello React</h1>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum
-                expedita facilis quas veritatis quo nemo ad, necessitatibus
-                repellendus, sit ipsum saepe ullam rem asperiores voluptas iusto
-                magnam officiis a minima!
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum
-                expedita facilis quas veritatis quo nemo ad, necessitatibus
-                repellendus, sit ipsum saepe ullam rem asperiores voluptas iusto
-                magnam officiis a minima!
-            </p>
-        </div>
+        <>
+            <Title />
+            <Content />
+        </>
     )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode>{<App />}</React.StrictMode>)
