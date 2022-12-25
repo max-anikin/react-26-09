9df1b42a44e7c37f7c6e95aef7ac21c4fbe421a1
Maksym_Anikin <max.anikin@gmail.com>
2022-12-25 21:29:42 +0200

    added Menu conponent. Buttons to Menu

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,10 +1,11 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
+
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import Container from '@mui/material/Container'
+import Menu from 'components/Menu/Menu'
 
 type Props = {}
 const Header = (props: Props) => {
@@ -29,7 +30,7 @@ const Header = (props: Props) => {
                         >
                             News
                         </Typography>
-                        <Button color="inherit">Login</Button>
+                        <Menu />
                     </Toolbar>
                 </Container>
             </AppBar>
