274f3b9eb1232387001674476489ac48e1387aa7
Maksym_Anikin <max.anikin@gmail.com>
2022-12-25 20:14:00 +0200

    added baseUrl in tsconfig.json, cleaned files to start new project

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Title } from './Title'
-
-type ContentProps = {
-    text1: string
-    text2: string
-}
-const Content = (props: ContentProps) => {
-    return (
-        <React.Fragment>
-            <p>{props.text1}</p>
-            <p>{props.text2}</p>
-        </React.Fragment>
-    )
-}
-
-const App = () => {
-    return (
-        <>
-            <Title title="React" />
-
-            <Title title="TS" />
-            <Title title={10} />
-            <Content text1="Paragraph 1" text2="Paragraph 2" />
-        </>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{<App />}</React.StrictMode>)
+root.render(<React.StrictMode></React.StrictMode>)
