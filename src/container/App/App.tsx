f5da25d49baad42e7030c07eb4f26766c7b73fcc
Maksym_Anikin <max.anikin@gmail.com>
2022-12-25 20:46:14 +0200

    added Material, CSSbaseLine

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,9 +1,11 @@
+import CssBaseline from '@mui/material/CssBaseline'
 import Header from 'container/Header/Header'
 
 type Props = {}
 const App = (props: Props) => {
     return (
         <>
+            <CssBaseline />
             <Header />
         </>
     )
