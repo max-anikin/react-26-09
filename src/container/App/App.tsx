fe87feeb2d92a06da8287e54468ea143c1ae3c79
Maksym_Anikin <max.anikin@gmail.com>
2022-12-25 20:26:07 +0200

    added App and Header

---
+++ src/container/App/App.tsx
@@ -0,0 +1,11 @@
+import Header from 'container/Header/Header'
+
+type Props = {}
+const App = (props: Props) => {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
+export default App
