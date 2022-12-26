74f6ad447a2047b56ba97c24ed9fecf4b18a90d4
Maksym_Anikin <max.anikin@gmail.com>
2022-12-26 13:59:30 +0200

    Использование CSS через модули

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -7,13 +7,13 @@ import Container from '@mui/material/Container'
 import Menu from 'components/Menu/Menu'
 import CartHeader from 'components/CartHeader/CartHeader'
 import Logo from 'components/Logo/Logo'
-import './Header.css'
+import classes from './Header.module.css'
 
 type Props = {}
 const Header = (props: Props) => {
     return (
         <>
-            <AppBar position="static" className="app-bar">
+            <AppBar position="static" className={`${classes['app-bar']}`}>
                 <Container maxWidth="lg">
                     <Toolbar>
                         <IconButton
