1d8e2eef5b106ea3a1671bc6cf5807687abcfd33
Maksym_Anikin <max.anikin@gmail.com>
2022-12-26 15:18:05 +0200

    Correction to previous commit. Add Main and Home

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,5 +1,12 @@
+import { Container } from '@mui/material'
+import Home from 'pages/Home/Home'
+
 type Props = {}
 const Main = (props: Props) => {
-    return <div>Main</div>
+    return (
+        <Container maxWidth="lg">
+            <Home />
+        </Container>
+    )
 }
 export default Main
