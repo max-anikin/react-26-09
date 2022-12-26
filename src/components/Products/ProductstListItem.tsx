d65d8d4b4ae496198e1606ffa6d03bb818ff870d
Maksym_Anikin <max.anikin@gmail.com>
2022-12-26 20:10:37 +0200

    Консервативный способ работы со State

--- src/components/Products/ProductstListItem.tsx
+++ src/components/Products/ProductstListItem.tsx
@@ -5,9 +5,7 @@ import {
     CardContent,
     TextField,
 } from '@mui/material'
-
 import { Component } from 'react'
-
 import './ProductsListItem.scss'
 
 type Props = {
@@ -19,35 +17,50 @@ type Props = {
     image: string
 }
 
-//
+type State = {
+    count: number
+}
 
-class ProductsListItem extends Component<Props> {
+class ProductsListItem extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.onIncrement = this.onIncrement.bind(this)
+        this.state = {
+            count: 1,
+        }
+    }
+
+    onIncrement() {
+        this.setState((prevState: State) => ({
+            count: prevState.count + 1,
+        }))
+    }
+
     render() {
+        const { title, desc, type, capacity, price, image } = this.props
         return (
             <Card className="product" variant="outlined">
                 <CardContent>
                     <div className="product-img">
-                        <img
-                            src={this.props.image}
-                            alt={this.props.title}
-                            title={this.props.title}
-                        />
+                        <img src={image} alt={title} title={title} />
                     </div>
-                    <div className="product-title">{this.props.title}</div>
-                    <div className="product-decs">{this.props.desc}</div>
+                    <div className="product-title">{title}</div>
+                    <div className="product-decs">{desc}</div>
                     <div className="product-features">
-                        <span>Type:</span> {this.props.type}
+                        <span>Type:</span> {type}
                     </div>
                     <div className="product-features">
-                        <span>Capacity:</span> {this.props.capacity}Gb
+                        <span>Capacity:</span> {capacity}Gb
                     </div>
                     <div className="product-price">
-                        <span>Price:</span> {this.props.price}$
+                        <span>Price:</span> {price}$
                     </div>
                     <div className="product-quantity">
                         <Button variant="outlined">-</Button>
-                        <TextField value="1" size="small" />
-                        <Button variant="outlined">+</Button>
+                        <TextField value={this.state.count} size="small" />
+                        <Button variant="outlined" onClick={this.onIncrement}>
+                            +
+                        </Button>
                     </div>
                 </CardContent>
                 <CardActions className="btns-wrap">
@@ -57,42 +70,5 @@ class ProductsListItem extends Component<Props> {
         )
     }
 }
-
-//
-
-// const ProductsListItem = (props: Props) => {
-//     return (
-//         <Card className="product" variant="outlined">
-//             <CardContent>
-//                 <div className="product-img">
-//                     <img
-//                         src={props.image}
-//                         alt={props.title}
-//                         title={props.title}
-//                     />
-//                 </div>
-//                 <div className="product-title">{props.title}</div>
-//                 <div className="product-decs">{props.desc}</div>
-//                 <div className="product-features">
-//                     <span>Type:</span> {props.type}
-//                 </div>
-//                 <div className="product-features">
-//                     <span>Capacity:</span> {props.capacity}Gb
-//                 </div>
-//                 <div className="product-price">
-//                     <span>Price:</span> {props.price}$
-//                 </div>
-//                 <div className="product-quantity">
-//                     <Button variant="outlined">-</Button>
-//                     <TextField value="1" size="small" />
-//                     <Button variant="outlined">+</Button>
-//                 </div>
-//             </CardContent>
-//             <CardActions className="btns-wrap">
-//                 <Button variant="outlined">Add to cart</Button>
-//             </CardActions>
-//         </Card>
-//     )
-// }
 
 export default ProductsListItem
