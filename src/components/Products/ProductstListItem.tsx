76fbf7a27e258a02e7c445a2c1f92526c1b45b93
Maksym_Anikin <max.anikin@gmail.com>
2022-12-28 18:33:44 +0200

    add Change Color button

--- src/components/Products/ProductstListItem.tsx
+++ src/components/Products/ProductstListItem.tsx
@@ -19,16 +19,30 @@ type Props = {
 
 type State = {
     count: number
+    color: string
 }
 
 class ProductsListItem extends Component<Props, State> {
     state = {
         count: 1,
+        color: 'green',
     }
 
-    onIncrement() {
+    onIncrement = () => {
         this.setState((prevState: State) => ({
             count: prevState.count + 1,
+        }))
+    }
+
+    onDecrement = () => {
+        this.setState((prevState: State) => ({
+            count: prevState.count - 1,
+        }))
+    }
+
+    changeColor = () => {
+        this.setState((prevState: State) => ({
+            color: prevState.color === 'green' ? 'red' : 'green',
         }))
     }
 
@@ -51,13 +65,18 @@ class ProductsListItem extends Component<Props, State> {
                     <div className="product-price">
                         <span>Price:</span> {price}$
                     </div>
+                    <div>Color: {this.state.color}</div>
+                    <button onClick={this.changeColor}>Change</button>
                     <div className="product-quantity">
-                        <Button variant="outlined">-</Button>
-                        <TextField value={this.state.count} size="small" />
                         <Button
                             variant="outlined"
-                            onClick={() => this.onIncrement()}
+                            onClick={this.onDecrement}
+                            disabled={this.state.count <= 1 ? true : false}
                         >
+                            -
+                        </Button>
+                        <TextField value={this.state.count} size="small" />
+                        <Button variant="outlined" onClick={this.onIncrement}>
                             +
                         </Button>
                     </div>
