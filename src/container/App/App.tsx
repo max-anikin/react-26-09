b6e5c1cfcdc0cb2cfbf5f7ee4520bac014c490fa
Maksym_Anikin <max.anikin@gmail.com>
2022-12-28 20:02:29 +0200

    Just save state before next task

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -2,9 +2,21 @@ import CssBaseline from '@mui/material/CssBaseline'
 import Header from 'container/Header/Header'
 import { StyledEngineProvider } from '@mui/material/styles'
 import Main from 'container/Main/Main'
+import { useState } from 'react'
 
 type Props = {}
+
+type CartData = {
+    totalCount: number
+    totalPrice: number
+}
+
 const App = (props: Props) => {
+    const [cartData, setCartData] = useState<CartData>({
+        totalCount: 0,
+        totalPrice: 0,
+    })
+
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
