df5bf3227c04476241708ac0f28f4dad2ca36e8f
Maksym_Anikin <max.anikin@gmail.com>
2023-01-04 20:28:18 +0200

    add react-router-dom. add pages. add links to pages in menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,15 +1,17 @@
-import Button from '@mui/material/Button'
+import MenuItem from './MenuItem'
 
 type Props = {}
+
 const Menu = (props: Props) => {
     return (
         <>
-            <Button color="inherit">Home</Button>
-            <Button color="inherit">Products</Button>
-            <Button color="inherit">Shipping</Button>
-            <Button color="inherit">Payment</Button>
-            <Button color="inherit">Cart</Button>
+            <MenuItem to="/">Home</MenuItem>
+            <MenuItem to="/about">About</MenuItem>
+            <MenuItem to="/shipping">Shipping</MenuItem>
+            <MenuItem to="/payment">Payment</MenuItem>
+            <MenuItem to="/cart">Cart</MenuItem>
         </>
     )
 }
+
 export default Menu
--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,5 +1,10 @@
 import { Container } from '@mui/material'
+import AboutPage from 'pages/AboutPage/AboutPage'
+import CartPage from 'pages/CartPage/CartPage'
 import Home from 'pages/Home/Home'
+import PaymentPage from 'pages/PaymentPage/PaymentPage'
+import ShippingPage from 'pages/ShippingPage/ShippingPage'
+import { Routes, Route } from 'react-router-dom'
 
 type Props = {
     addProductToCart: (id: number, count: number) => void
@@ -7,7 +12,16 @@ type Props = {
 const Main = ({ addProductToCart }: Props) => {
     return (
         <Container maxWidth="lg" sx={{ padding: '50px 0' }} component="main">
-            <Home addProductToCart={addProductToCart} />
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Home addProductToCart={addProductToCart} />}
+                />
+                <Route path="about" element={<AboutPage />} />
+                <Route path="shipping" element={<ShippingPage />} />
+                <Route path="payment" element={<PaymentPage />} />
+                <Route path="cart" element={<CartPage />} />
+            </Routes>
         </Container>
     )
 }
